41171bfa215680f245cc69d989193a125e392575
kuma <kumaleta2021@gmail.com>
2025-06-17 08:45:59 +0300

    routes routes for frontend added

---
+++ frontend/src/ROUTES.js
@@ -0,0 +1,12 @@
+const ROUTES = {
+  HOME: "/",
+  LOGIN: "/login",
+  SIGNUP: "/signup",
+  DASHBOARD: "/app/dashboard",
+  TODOS: "/app/todos",
+  COMPLETED: "/app/completed",
+  IN_PROGRESS: "/app/in-progress",
+  TEAM: "/app/team",
+};
+
+export default ROUTES;
